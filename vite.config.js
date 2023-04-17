885ff833416ad20d74d1912792f0bdfa9c7305af
Aki <13302223+aki-mia@users.noreply.github.com>
2023-04-17 12:52:35 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [reactRefresh()],
-};
+export default defineConfig({
+  plugins: [react()]
+});
